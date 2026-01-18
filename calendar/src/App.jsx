b3b0ad4d52d91a9db0e8b2818bf752cf205d15db
ElinaHordynska <additional.elina@gmail.com>
2026-01-18 15:40:23 +0200

    Зробили втсвітлення помилки

--- calendar/src/App.jsx
+++ calendar/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
+import AddForm from './components/AddForm/AddForm'
+
 
 export default function App() {
   return (
     <>
       <Header/>
+      <Main/>
+      <Footer/>
+      <AddForm/>
     </>
   )
 }
